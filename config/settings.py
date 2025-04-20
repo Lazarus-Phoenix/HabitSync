fe0cf8feecb4b580fa1fa4c7efc254f0f639feb6
dima <dmitrij-bezgubov@yandex.ru>
2025-04-21 00:33:35 +0500

    Оказывается dotenv забыл подгрузить в settings.py пока всё работает и с такой организацией окружения.

--- config/settings.py
+++ config/settings.py
@@ -15,14 +15,14 @@ from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv(override=True)
+# load_dotenv(override=True)
 
 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / '.env', override=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
